d8fef0f3be1c5e7f16b110fb84aa63ef5100a4bd
Kobamiyannnn <kobaprfm23@gmail.com>
2023-02-12 19:35:55 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//  
+//  Created by Kobamiyannnn on 2023/02/12
+//  
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
