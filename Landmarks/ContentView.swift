551b933fcc4d4712d6fb0e29dd132ecc6ada3fca
Kobamiyannnn <kobaprfm23@gmail.com>
2023-02-13 22:05:02 +0900

    Section 2 of Chapter 1 completed

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -14,13 +14,9 @@ import SwiftUI
  */
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("My first iOS app!")
-        }
-        .padding()
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(.black)
     }
 }
 
