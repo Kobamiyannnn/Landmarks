9c8a2c340089912a0af45cac3c4306b4324d312c
Kobamiyannnn <kobaprfm23@gmail.com>
2023-02-13 23:54:19 +0900

    Section 6 of Chapter 1 completed

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -14,18 +14,40 @@ import SwiftUI
  */
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top) // Allow the map content to extend to the top edge of the screen
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130) // Layer the image view on top of the map view.
+                .padding(.bottom, -130) 
+            
+            // Left-align this VStack by "alignment: .leading".
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding() // Padding top, bottom, left and right.
+            
+            Spacer() // Push the content to the top of the screen.
         }
-        .padding()
     }
 }
 
